a1325982a37a0a32292db96d685e23f11810e996
siddhant <siddhant@systeminsights.com>
2020-10-24 20:45:52 +0530

    added mock data

---
+++ src/components/Api/mocks/index.ts
@@ -0,0 +1,46 @@
+import { LoremIpsum } from "lorem-ipsum";
+import { Contact } from "../../ContactList/types";
+import { MessageList } from "../../ChatScreen/types";
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 5,
+    min: 3
+  },
+  wordsPerSentence: {
+    max: 6,
+    min: 3
+  }
+});
+
+export const defaultContactNames: Array<string> = ["Siddhant", "Sid", "Al", "Kimberly", "Nehal", "Ashley", "Ishika", "Ashish"];
+
+export const getDefaultContactList = () : Array<Contact> => {
+  return defaultContactNames.map((name: string, index: number) => ({
+    defaultName: name,
+    id: `${index + 1}`,
+    number: 1234567890
+  }));
+}
+
+export const getDefaultChatMessages = (contactId: string, currentUserId: string): MessageList => {
+  const returnValues = Array.from({ length: 5 }, (_, index) => {
+    const message = {
+      authorId: currentUserId,
+      message: lorem.generateSentences(index * 2),
+      type: "string"
+    }
+    if (index % 2 === 0) {
+      return {
+        ...message,
+        id: `${index + 1}`,  
+      }
+    }
+
+    return {
+      ...message,
+      id: `${index + 1}`
+    }
+  })
+  return returnValues
+}
